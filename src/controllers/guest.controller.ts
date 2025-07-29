3bb59e97ad2bd0e717611d20bd4bce43a6854d4b
adwaith-parkmate <148951510+adwaith-parkmate@users.noreply.github.com>
2025-07-29 11:23:18 +0530

    initial-preview

--- src/controllers/guest.controller.ts
+++ src/controllers/guest.controller.ts
@@ -1,29 +1,29 @@
 import { Response, NextFunction } from "express";
-import { getInvitedGuestsService } from "@services/guest.service";
+// import {  } from "@services/guest.service";
 import { sendResponse } from "@utils/express.util";
 import { injectedRequest } from "types/injected-types";
 
 /**
  * Get the list of invited guests for a share token
  */
-export const getInvitedGuestsController = async (req: injectedRequest, res: Response, next: NextFunction) => {
-    try {
-        const { token_id } = req.params;
-        const userId = req.user._id.toString();
+// export const getInvitedGuestsController = async (req: injectedRequest, res: Response, next: NextFunction) => {
+//     try {
+//         const { token_id } = req.params;
+//         const userId = req.user._id.toString();
         
-        // Get the guest list
-        const response = await getInvitedGuestsService(token_id, userId);
+//         // Get the guest list
+//         const response = await getInvitedGuestsService(token_id, userId);
         
-        // Send response
-        return sendResponse(res, response);
-    } catch (error: any) {
-        return sendResponse(res, {
-            status: false,
-            code: 500,
-            message: "Failed to retrieve invited guests",
-            data: null,
-            error: { message: error.message },
-            other: null
-        });
-    }
-};
+//         // Send response
+//         return sendResponse(res, response);
+//     } catch (error: any) {
+//         return sendResponse(res, {
+//             status: false,
+//             code: 500,
+//             message: "Failed to retrieve invited guests",
+//             data: null,
+//             error: { message: error.message },
+//             other: null
+//         });
+//     }
+// };
